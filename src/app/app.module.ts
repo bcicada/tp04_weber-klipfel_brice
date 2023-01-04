d39dc8b8bf3109d9420ccd8d2fb2ffd94718dc99
bcicada <b.cicada@protonmail.com>
2023-01-04 20:44:39 +0000

    Adding content and aside with SCSS associate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
